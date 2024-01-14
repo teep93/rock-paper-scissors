148c40cf7b01cbc90ad405bd63729e59aeefafc7
Rorry <153604881+teep93@users.noreply.github.com>
2024-01-14 22:40:29 +0800

    add skeleton and comments of project goal

---
+++ script.js
@@ -0,0 +1,19 @@
+//begin with function called getComputerChoice
+//that will randomly return "rock", "paper" or "scissors"
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+//then a function that plays a single round of RPS
+//the function should take 2 parameters
+//playerSelection and computerSelection
+
+
+//return a string that declares a winner of round
+//make it case insensitive
+//account for TIES be re-playing round
+
+//return results of this function call not console.log it
