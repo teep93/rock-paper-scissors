affe4f10090086a77f69a1e151dd569cbc38a5fe
Rorry <153604881+teep93@users.noreply.github.com>
2024-01-16 21:17:58 +0800

    add game round function

--- script.js
+++ script.js
@@ -9,16 +9,67 @@ function getComputerChoice() {
   let getComputerChoiceArr = [getComputerChoice_one, getComputerChoice_two, getComputerChoice_three];
   let random = getComputerChoiceArr[Math.floor(Math.random() * getComputerChoiceArr.length)]
 
-  return random 
+  return random;
 }
 
 
 
 
 function playRound(playerSelection, computerSelection) {
-    // your code here!
-  }
-   
+  if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper")
+
+    console.log("You lose! Paper beats Rock!");
+
+
+
+  else if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors")
+
+    console.log("You Win! Rock beats Scissors!");
+
+
+
+  else if (playerSelection.toLowerCase() == "rock" && computerSelection == "rock")
+
+    console.log("It's a TIE, try again.");
+
+
+
+  else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock")
+
+    console.log("You win! Paper beats Rock!");
+
+
+
+  else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors")
+
+    console.log("You lose! Scissors beats Paper!");
+
+
+
+  else if (playerSelection.toLowerCase() == "paper" && computerSelection == "paper")
+
+    console.log("It's a TIE, try again.");
+
+
+
+  else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock")
+
+    console.log("You lose! Rock beats Scissors!");
+
+
+
+  else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper")
+
+    console.log("You win! Scissors beats Paper!");
+
+
+
+  else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "scissors")
+
+    console.log("It's a TIE, try again.");
+}
+
+
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
